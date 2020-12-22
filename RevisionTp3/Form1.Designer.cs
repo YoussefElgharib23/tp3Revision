65825116f0767864d36db000c9f8eff0a302f98c
YoussefElgharib23 <youssef.elgharib17@gmail.com>
2020-12-22 19:58:31 +0100

    Fonction d'ajouter

---
+++ RevisionTp3/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace RevisionTp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxStagiaires = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxPrenom = new System.Windows.Forms.TextBox();
+            this.textBoxNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.textBoxTel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxStagiaires
+            // 
+            this.listBoxStagiaires.FormattingEnabled = true;
+            this.listBoxStagiaires.ItemHeight = 16;
+            this.listBoxStagiaires.Location = new System.Drawing.Point(12, 12);
+            this.listBoxStagiaires.Name = "listBoxStagiaires";
+            this.listBoxStagiaires.Size = new System.Drawing.Size(599, 292);
+            this.listBoxStagiaires.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAjouter);
+            this.groupBox1.Controls.Add(this.textBoxTel);
+            this.groupBox1.Controls.Add(this.textBoxNom);
+            this.groupBox1.Controls.Add(this.textBoxPrenom);
+            this.groupBox1.Controls.Add(this.textBoxNum);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 310);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(599, 167);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ajout";
+            // 
+            // textBoxPrenom
+            // 
+            this.textBoxPrenom.Location = new System.Drawing.Point(100, 64);
+            this.textBoxPrenom.Name = "textBoxPrenom";
+            this.textBoxPrenom.Size = new System.Drawing.Size(165, 22);
+            this.textBoxPrenom.TabIndex = 2;
+            // 
+            // textBoxNum
+            // 
+            this.textBoxNum.Location = new System.Drawing.Point(100, 23);
+            this.textBoxNum.Name = "textBoxNum";
+            this.textBoxNum.Size = new System.Drawing.Size(165, 22);
+            this.textBoxNum.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Prenom:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero: ";
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(392, 23);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(165, 22);
+            this.textBoxNom.TabIndex = 3;
+            // 
+            // textBoxTel
+            // 
+            this.textBoxTel.Location = new System.Drawing.Point(392, 64);
+            this.textBoxTel.Name = "textBoxTel";
+            this.textBoxTel.Size = new System.Drawing.Size(165, 22);
+            this.textBoxTel.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nom:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Telephone:";
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(239, 116);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(112, 30);
+            this.btnAjouter.TabIndex = 5;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.eventClickHandle);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 738);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBoxStagiaires);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formClosingEventHandle);
+            this.Load += new System.EventHandler(this.loadEventHandle);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxStagiaires;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxPrenom;
+        private System.Windows.Forms.TextBox textBoxNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.TextBox textBoxTel;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RevisionTp3/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RevisionTp3
+{
+    public partial class Form1 : Form
+    {
+        // A FAIRE CHANGER LE DATA SOURCE ET INITIAL CATALOG A VOTRE PROPRE DONNEES
+        static string connectionString = @"Data Source=DESKTOP-S7OEGIL;Initial Catalog=GStagiaireRevision;Integrated Security=True";
+        static SqlConnection connection = new SqlConnection(connectionString);
+        SqlCommand command = new SqlCommand("", connection);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void eventClickHandle(object sender, EventArgs e)
+        {
+            string prenom = textBoxPrenom.Text,
+                   nom = textBoxNom.Text,
+                   telephone = textBoxTel.Text;
+            int numero = -1;
+            if ( !int.TryParse(textBoxNum.Text, out numero) )
+            {
+                MessageBox.Show("Le format du numero est invalid !");
+                return;
+            }
+            // N"OUBLIZ PAS A CHANGER LE NOM DU TABLE
+            command.CommandText = "INSERT INTO stagiaires VALUES (@numero, @nom, @prenom, @telephone)";
+            command.Parameters.AddWithValue("@numero", numero);
+            command.Parameters.AddWithValue("@nom", nom);
+            command.Parameters.AddWithValue("@prenom", prenom);
+            command.Parameters.AddWithValue("@telephone", telephone);
+
+            if (command.ExecuteNonQuery() > 0)
+            {
+                command.Parameters.Clear();
+                MessageBox.Show("Les donnew sont bien inseree !");
+                actualiserListBox();
+            }
+        }
+
+        private void actualiserListBox()
+        {
+            command.CommandText = "SELECT * FROM stagiaires";
+            SqlDataReader dataReader = command.ExecuteReader();
+            if ( dataReader.HasRows )
+            {
+                listBoxStagiaires.Items.Clear();
+                while ( dataReader.Read() )
+                {
+                    listBoxStagiaires.Items.Add(string.Format(dataReader[0] + ": " + dataReader[1] + " " + dataReader[2] + ", " + dataReader[3]));
+                }
+            }
+            dataReader.Close();
+        }
+
+        private void loadEventHandle(object sender, EventArgs e)
+        {
+            connection.Open();
+            actualiserListBox();
+        }
+
+        private void formClosingEventHandle(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+        }
+    }
+}
